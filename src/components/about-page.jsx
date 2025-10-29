d87b8358bccc6f99472f5eeec64302bd6bd9200c
TR1432 <ppfaddy@gmail.com>
2025-10-29 14:01:07 +0100

    Feat: Added ICon and Updated some data

--- src/components/about-page.jsx
+++ src/components/about-page.jsx
@@ -1,69 +1,98 @@
-import React from 'react'
-import FlowInText from './flow-in-text'
-import { motion, useInView } from 'motion/react'
-import { useRef } from 'react'
+import React from "react";
+import FlowInText from "./flow-in-text";
+import { motion, useInView } from "motion/react";
+import { useRef } from "react";
 const AboutPage = () => {
-  let mainText = "I BUILD BEAUTIFUL WEBSITES, SCALABLE APPLICATIONS, AND LASTING TECH SOLUTIONS."
-  
+  let mainText =
+    "I BUILD BEAUTIFUL WEBSITES, SCALABLE APPLICATIONS, AND LASTING TECH SOLUTIONS.";
+
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true })
+  const isInView = useInView(ref, { once: true });
 
   return (
-    <section id='AboutSection' className='max-w-screen w-screen mt-[4vw] lg:mt-[10vw] md:mt-[1vh] h-fit font-mono grid grid-cols-12 gap-[25px] px-[8vw] overflow-hidden'>
-      <h1 className='col-span-12 text-[17px] md:text-[28px] lg:text-[40px] text-accent-2 text-center mb-8 lg:mb-18 font-montserrat font-bold'>ABOUT ME</h1>
-      <FlowInText text={mainText}/>
-      <div className='col-span-12 lg:col-span-8 text-[18px] md:text-[24px] lg:text-[30px]'>
-        <motion.div className=''
+    <section
+      id="AboutSection"
+      className="max-w-screen w-screen mt-[4vw] lg:mt-[10vw] md:mt-[1vh] h-fit font-mono grid grid-cols-12 gap-[25px] px-[8vw] overflow-hidden"
+    >
+      <h1 className="col-span-12 text-[17px] md:text-[28px] lg:text-[40px] text-accent-2 text-center mb-8 lg:mb-18 font-montserrat font-bold">
+        ABOUT ME
+      </h1>
+      <FlowInText text={mainText} />
+      <div className="col-span-12 lg:col-span-8 text-[18px] md:text-[24px] lg:text-[30px]">
+        <motion.div
+          className=""
           ref={ref}
           initial={{ opacity: 0, y: 50 }}
-          animate={ isInView ? { opacity: 1, y: 0 } : {}}
-          transition={{ duration: .25}}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.25 }}
         >
-          I specialize in building modern, scalable web applications and engaging digital experiences that bring ideas to life. My passion lies in using technology to empower businesses, brands, and projects, creating solutions that are not only functional but also impactful.  
+          I specialize in building modern, scalable web applications and
+          engaging digital experiences that bring ideas to life. My passion lies
+          in using technology to empower businesses, brands, and projects,
+          creating solutions that are not only functional but also impactful.
         </motion.div>
-        <motion.div className='my-5'
+        <motion.div
+          className="my-5"
           initial={{ opacity: 0, y: 50 }}
-          animate={ isInView ? { opacity: 1, y: 0} : {}}
-          transition={{ duration: .25, delay: .2 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.25, delay: 0.2 }}
         >
-          Beyond my work as a full-stack developer, I have a deep appreciation for art, ancient philosophy, and gaming, which often inspire my creative approach to problem-solving. When I’m not coding, you’ll likely find me leveling up my COD: Warzone weapons or analyzing hip-hop lyricism.
+          Beyond my work as a full-stack developer, I have a deep appreciation
+          for art, ancient philosophy, and gaming, which often inspire my
+          creative approach to problem-solving. When I’m not coding, you’ll
+          likely find me leveling up my COD: Warzone weapons or analyzing
+          hip-hop lyricism.
         </motion.div>
-        <motion.div className=''
+        <motion.div
+          className=""
           initial={{ opacity: 0, y: 50 }}
-          animate={ isInView ? { opacity: 1, y: 0} : {}}
-          transition={{ duration: .25, delay: .4 }}
+          animate={isInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.25, delay: 0.4 }}
         >
-          I'm working on exciting projects that I can't wait to share. But I'm always open to new challenges, collaborations, and opportunities to create something amazing.
+          I'm working on exciting projects that I can't wait to share. But I'm
+          always open to new challenges, collaborations, and opportunities to
+          create something amazing.
         </motion.div>
       </div>
-      <div className='col-span-12 lg:col-span-4 text-[18px] md:text-[24px] font-light lg:text-[30px] '>
+      <div className="col-span-12 lg:col-span-4 text-[18px] md:text-[24px] font-light lg:text-[30px] ">
         <motion.div
           initial={{ opacity: 0, x: 50 }}
-          animate={ isInView ? { opacity: 1, x: 0} : {}}
-          transition={{ duration: .25, delay: .2 }}
+          animate={isInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.25, delay: 0.2 }}
         >
-          <h1 className='font-bold text-accent-2'>Frontend Tools</h1>
-          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">React, Next.js, Vitetest, HTML5, CSS/SCSS/SASS, TailwindCSS, Framer, Redux Toolkit, Redux.</div>
+          <h1 className="font-bold text-accent-2">Frontend Tools</h1>
+          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">
+            React, Next.js, Vitetest, HTML5, CSS/SCSS, TailwindCSS, Framer,
+            Redux Toolkit, Redux.
+          </div>
         </motion.div>
-        <motion.div className='mt-5'
+        <motion.div
+          className="mt-5"
           initial={{ opacity: 0, x: 50 }}
-          animate={ isInView ? { opacity: 1, x: 0} : {}}
-          transition={{ duration: .25, delay: .2 }}
+          animate={isInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.25, delay: 0.2 }}
         >
-          <h1 className='font-bold text-accent-2'>Backend Tools</h1>
-          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">Node.js, Express.js, Python, FastAPi, MongoDB, Mongoose, Postgres, Prisma.</div>
+          <h1 className="font-bold text-accent-2">Backend Tools</h1>
+          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">
+            Node.js, Express.js, Nest.js, Python, FastAPI, MongoDB, Mongoose,
+            Postgres, Prisma.
+          </div>
         </motion.div>
-        <motion.div className='mt-5'
+        <motion.div
+          className="mt-5"
           initial={{ opacity: 0, x: 50 }}
-          animate={ isInView ? { opacity: 1, x: 0} : {}}
-          transition={{ duration: .25, delay: .2 }}
+          animate={isInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 0.25, delay: 0.2 }}
         >
-          <h1 className='font-bold text-accent-2'>Interests</h1>
-          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">Programming(Obviously), Sales and Marketing, Ancient Philosophy, Gaming, New Tech, Science and Engineering.</div>
+          <h1 className="font-bold text-accent-2">Interests</h1>
+          <div className="font-light text-[15px] md:text-[20px] lg:text-[25px]">
+            Programming(Obviously), Sales and Marketing, Ancient Philosophy,
+            Gaming, New Tech, Science and Engineering.
+          </div>
         </motion.div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- src/components/data.js
+++ src/components/data.js
@@ -1,65 +1,73 @@
 export const skills = [
-        {
-          name: 'Frontend Developer',
-          tools: 'React | Next.js | Framer | Javascript | Tailwind'
-        },{
-          name: 'Backend Developer',
-          tools: 'Python | Nodejs | TypeScript | FastApi'
-        },
-      ];
-      
-export const contactInfos= [
-        {
-          link: "https://github.com/TR1432",
-          icon: "fa-brands fa-github",
-          title: "My GitHub"
-      },{
-          link: 'https://x.com/payomi_kun',
-          icon: 'fa-brands fa-x-twitter',
-          title: 'My Twitter'
-      },{
-        link: 'https://www.instagram.com/ayo.mikun_/',
-        icon: 'fa-brands fa-instagram',
-        title: 'My Instagram'
-      },{
-        link: 'https://www.linkedin.com/in/oluwapelumi-fadahunsi-3a6174294/',
-        icon: 'fa-brands fa-linkedin-in',
-        title: 'My LinkedIn'
-      },{
-        link: 'mailto:ppfaddy@gmail.com',
-        icon: 'fa-regular fa-envelope',
-        title: 'My Gmail'
-      }]
+  {
+    name: "Frontend Developer",
+    tools: "React | Next.js | Framer | Javascript | Tailwind",
+  },
+  {
+    name: "Backend Developer",
+    tools: "Python | Nodejs | TypeScript | FastApi",
+  },
+];
+
+export const contactInfos = [
+  {
+    link: "https://github.com/pelumibuildzz",
+    icon: "fa-brands fa-github",
+    title: "My GitHub",
+  },
+  {
+    link: "https://x.com/payomi_kun",
+    icon: "fa-brands fa-x-twitter",
+    title: "My Twitter",
+  },
+  {
+    link: "https://www.instagram.com/ayo.mikun_/",
+    icon: "fa-brands fa-instagram",
+    title: "My Instagram",
+  },
+  {
+    link: "https://www.linkedin.com/in/oluwapelumi-fadahunsi-3a6174294/",
+    icon: "fa-brands fa-linkedin-in",
+    title: "My LinkedIn",
+  },
+  {
+    link: "mailto:pelumifadahunsi30@gmail.com",
+    icon: "fa-regular fa-envelope",
+    title: "My Gmail",
+  },
+];
 
 export const projectList = [
   {
     img: "/images/fsl.jpeg",
     title: "FSL",
-    description: "Fantasy Football Web-App for Student Work Experiece Programme.",
+    description:
+      "Fantasy Football Web-App for Student Work Experiece Programme.",
     links: {
       git: "https://github.com/pelumibuildzz/FSL",
     },
     icons: "HTMLCssJsBstrapFlask",
-    year: "2024"
+    year: "2024",
   },
   {
     img: "/images/booking-api.png",
     title: "Booking System Api",
-    description: "A REST Api to create and manage booking events in auditotiums.",
+    description:
+      "A REST Api to create and manage booking events in auditotiums.",
     links: {
       git: "https://github.com/pelumibuildzz/Booking-System-Api",
     },
     icons: "NodeExpMong",
-    year: "2025"
+    year: "2025",
   },
   {
     img: "/images/echo.jpg",
     title: "Echo Api",
     description: "Rest Api for a Petition App in Covenant University.",
     links: {
-      git: "https://github.com/pelumibuildzz/Echo-API"
+      git: "https://github.com/pelumibuildzz/Echo-API",
     },
     icons: "NodeExpMong",
-    year: "2025"
-  }
-]
+    year: "2025",
+  },
+];
